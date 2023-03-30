98766b670aa05b3948435e184610d0f33d596c08
João Alberto E Yasmin <joaoalbertomartinsbb@gmail.com>
2023-03-30 10:45:25 -0300

    Alterações leves ao código na pasta services

--- Biblioteca-Virtual/services/BibliService.cs
+++ Biblioteca-Virtual/services/BibliService.cs
@@ -8,9 +8,79 @@ public class BibliService : IBilbiService
     private readonly IHttpContextAccessor _session; 
     private readonly string LivroFile = @"Data\Livros.json"; 
     private readonly string GenerosFile = @"Data\generos.json"; 
-    public class BibliService(IHttpContextAccessor session)
+    public BibliService(IHttpContextAccessor session)
     {
         _session = session; 
         PopularSessao(); 
+    }
+    
+    public List<Livro> GetLivros()
+    {
+        PopularSessao(); 
+        var Livros = JsonSerializer.Deserialize<List<Livro>>
+            (_session.HttpContext.Session.GetString("Livros")); 
+        return  Livros; 
+    }
+
+    public List<Genero> GetGeneros()
+    {
+        PopularSessao(); 
+        var generos = JsonSerializer.Deserialize<List<Genero>>
+            (_session.HttpContext.Session.GetString("Gêneros"));
+        return generos; 
+    }
+
+    public Livro GetLivro(int Numero)
+    {
+      var Livro = GetLivros(); 
+      return Livro.Where(p => p.Numero == Numero).FirstOrDefault(); 
+    }
+
+    public bibliotecaDto GetBibliotecaDto()
+    {
+        var Liv = new bibliotecaDto()
+        {
+            Livros = GetLivros(), 
+            Generos = GetGeneros()
+        }; 
+        return Liv; 
+    }
+
+    public DetailsDto GetDetailedLivro(int Numero)
+    {
+        var Livros = GetLivros(); 
+        var Liv = new DetailsDto()
+        {
+            Current = Livros.Where(p => p.Numero == Numero).FirstOrDefault(), 
+            Prior = Livros.OrderByDescending(p => p.Numero).FirstOrDefault(p => p.Numero < Numero), 
+            Next = Livros.OrderBy(p => p.Numero).FirstOrDefault(p => p.Numero > Numero), 
+        }; 
+        return Liv;     
+    }
+    
+    public Genero GetGenero(string Nome)
+    {
+        var generos = GetGeneros(); 
+        return generos.Where(t => t.Nome == Nome).FirstOrDefault();  
+    }
+
+    private void PopularSessao()
+    {
+        if (string.IsNullOrEmpty(_session.HttpContext.Session.GetString("Generos")))
+        {
+            _session.HttpContext.Session
+              .SetString("Livros", LerArquivo(LivroFile));
+            _session.HttpContext.Session
+              .SetString("Generos", LerArquivo(GenerosFile));
+        }
+    }
+
+    private string LerArquivo(string filename)
+    {
+        using (StreamReader leitor = new StreamReader (filename))
+        {
+            string dados = leitor.ReadToEnd(); 
+            return dados; 
+        }
     }
 }
--- Biblioteca-Virtual/services/IBilbiService.cs
+++ Biblioteca-Virtual/services/IBilbiService.cs
@@ -3,18 +3,14 @@ namespace Biblioteca_Virtual.services;
 
 public interface IBilbiService
 {
-    public class IBilbiService
-    {
         List<Livro> GetLivros(); 
         List<Genero> GetGeneros();
-        Livro GetPokemon(int Numero);
+        Livro GetLivro(int Numero);
         bibliotecaDto GetBibliotecaDto(); 
 
         DetailsDto GetDetailedLivro(int Numero); 
 
         Genero GetGenero(string Nome);  
-
-    }
 }
 
 
